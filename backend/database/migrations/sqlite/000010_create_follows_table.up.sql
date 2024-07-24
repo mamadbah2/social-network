e74c7b2088a9238144cf10f7879b480b4a5fcc13
cnzale <Azertdev>
2024-07-24 14:16:37 +0000

    follows and group_member table updated

--- backend/database/migrations/sqlite/000010_create_follows_table.up.sql
+++ backend/database/migrations/sqlite/000010_create_follows_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE follows (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     id_follower INTEGER NOT NULL,
     id_followed INTEGER NOT NULL,
+    archived BOOLEAN DEFAULT FALSE,
     created_at DATETIME NOT NULL,
     FOREIGN KEY (id_follower) REFERENCES users(id),
     FOREIGN KEY (id_followed) REFERENCES users(id),
