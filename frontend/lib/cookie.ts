08bd60d19a77d26f2a1b20d81dd0b12c3c2319fd
mamadbah2 <bahmamadoubobosewa@gmail.com>
2024-08-20 17:36:47 +0000

    js-cookie install

--- frontend/lib/cookie.ts
+++ frontend/lib/cookie.ts
@@ -2,15 +2,15 @@ import Cookies from "js-cookie";
 
 // Fonction pour obtenir le token de session
 export const getSessionToken = (): string | undefined => {
-  return Cookies.get("token");
+  return Cookies.get("session");
 };
 
 // Fonction pour définir le token de session
 export const setSessionToken = (token: string): void => {
-  Cookies.set("token", token, { expires: 7 });
+  Cookies.set("session", token, { expires: 7 });
 };
 
 // Fonction pour supprimer le token de session
 export const removeSessionToken = (): void => {
-  Cookies.remove("token");
+  Cookies.remove("session");
 };
