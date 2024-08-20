42d82c091bc4a28ddffe79c97c58f9518bdd3211
mamadbah2 <bahmamadoubobosewa@gmail.com>
2024-08-20 17:31:10 +0000

    bug front

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,11 +3,8 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import SecurityLayout from "../securelayout"
-import useGetData from "@/lib/hooks/useget"
-import usePostData from "@/lib/hooks/usepost"
 
 export default function Login() {
-    const {data, error} = useGetData('/posts')
 
     return <SecurityLayout>
         <form className="max-w-[500px] flex flex-wrap gap-4 bg-white p-5 justify-center rounded-lg" >
