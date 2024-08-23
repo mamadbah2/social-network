db3710a9f6e3c5327b06cd4a7e51523407e0be2f
mamadbah2 <bahmamadoubobosewa@gmail.com>
2024-08-23 12:40:56 +0000

    validate beta suggest friend

--- frontend/lib/hooks/useget.ts
+++ frontend/lib/hooks/useget.ts
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { resp } from "@/models/resp.model"
-import { User } from "@/models/user.model"
 import useSWR from "swr"
-import { mapUser } from "../modelmapper"
-=======
-'use client'
-
-import useSWR from "swr";
->>>>>>> 131b734914d860342830de5b6773b3bbd9f5cadb
 
 const fetcher = async (url: string): Promise<any> => {
 
-<<<<<<< HEAD
     const data = await fetch(url, { credentials: 'include' })
 
     return data.json()
@@ -26,11 +16,6 @@ const useGetData = <T>(uri: string, mapper:(obj:any)=>T ) => {
 
     // const expect= mapper(data?.Datas)
     const expect = data?.Datas ? mapper(data?.Datas) : [];
-=======
-const useGetData = (url:string) => {
-    console.log(url)
-    const { data, error, isLoading} = useSWR(url, fetcher)
->>>>>>> 131b734914d860342830de5b6773b3bbd9f5cadb
 
     return {
         expect,
