4ad37e233372845a4a2451b37b2357a5534e706b
mamadbah2 <bahmamadoubobosewa@gmail.com>
2024-08-26 18:36:36 +0000

    backup

--- backend/cmd/web/handlers/notification.handler.go
+++ backend/cmd/web/handlers/notification.handler.go
@@ -1,7 +1,78 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"social-network/internal/models"
+
+	"github.com/gorilla/websocket"
+)
+
+var notifClients = make(map[int]*websocket.Conn)
 
 func (hand *Handler) Notification(w http.ResponseWriter, r *http.Request) {
-	
+	session, err := hand.ConnDB.GetSession(r)
+	if err != nil {
+		hand.Helpers.ServerError(w, err)
+		return
+	}
+	// h.Helpers.ErrorLog.Fatalln("session. =>>", session)
+	senderID := session.UserId
+
+	// Switching Protocol...
+	senderConn, err := socket.Upgrade(w, r, nil)
+	if err != nil {
+		hand.Helpers.ServerError(w, err)
+		return
+	}
+	defer senderConn.Close()
+
+	notifClients[senderID] = senderConn // Add sender's connection in the chat box.
+
+	// Get the message history from the database.
+	myNotifs, err := hand.ConnDB.GetNotifications(senderID)
+	if err != nil {
+		hand.Helpers.ServerError(w, err)
+		return
+	}
+
+	// Send the message history to the sender.
+	if err = senderConn.WriteJSON(myNotifs); err != nil {
+		hand.Helpers.ServerError(w, err)
+		return
+	}
+
+	for {
+		// Receive a new notif
+		var newNotif *models.Notification
+		// var strMsg string
+		if err := senderConn.ReadJSON(&newNotif); err != nil {
+			senderConn.Close()
+			delete(notifClients, senderID)
+			break
+		}
+
+		if newNotif.Approuved == true {
+			err := hand.ConnDB.ArchivedNotification(newNotif.Id)
+			if err != nil {
+				hand.Helpers.ClientError(w, http.StatusBadRequest)
+				return
+			}
+		} else {
+			_, err := hand.ConnDB.SetNotification(newNotif)
+			if err != nil {
+				hand.Helpers.ClientError(w, http.StatusBadRequest)
+				return
+			}
+			// Send the new messages to the receiver
+			// if the receiver has a connection in the chat box.
+			if receiverConn, exists := notifClients[newNotif.ReceiverID]; exists {
+				if err = receiverConn.WriteJSON(newNotif); err != nil {
+					receiverConn.Close()
+					delete(notifClients, newNotif.ReceiverID)
+					break
+				}
+			}
+		}
+
+	}
 }
---
+++ backend/internal/models/security.model.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"social-network/cmd/web/validators"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (m *ConnDB) Authenticate(emailOrUsername, password string) (int, error) {
+	var id int
+	var passwordeu string
+	var stmt string
+	if validators.Matches(emailOrUsername, validators.EmailRX) {
+		stmt = `SELECT id, password FROM users WHERE email = ?`
+	} else {
+		stmt = `SELECT id, password FROM users WHERE nickname = ?`
+	}
+	err := m.DB.QueryRow(stmt, emailOrUsername).Scan(&id, &passwordeu)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Println("error 1")
+			return 0, errors.New("models: invalid credentials")
+		} else {
+			fmt.Println("error 2")
+			return 0, err
+		}
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(passwordeu), []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, errors.New("models: invalid credentials")
+		} else {
+			fmt.Println("error 4")
+			return 0, err
+		}
+	}
+	return id, nil
+
+}
+
+func (m *ConnDB) CheckNickname(nickname string) (bool, error) {
+	var record int
+	// Prepare a query to check if the username exists
+	stmt := `SELECT count(*) FROM users WHERE nickname = ?`
+	err := m.DB.QueryRow(stmt, nickname).Scan(&record)
+	if err != nil {
+		return false, err
+	}
+	return record == 0, nil
+}
+
+func (m *ConnDB) CheckEmail(email string) (bool, error) {
+	var record int
+	// Prepare a query to check if the username exists
+	stmt := `SELECT count(*) FROM users WHERE email = ?`
+	err := m.DB.QueryRow(stmt, email).Scan(&record)
+	if err != nil {
+		return false, err
+	}
+	return record == 0, nil
+}
