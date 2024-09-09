250d25a882a2f47b15312411056ffe4e36b37827
mamadbah2 <bahmamadoubobosewa@gmail.com>
2024-09-09 22:59:42 +0000

    fka

--- backend/database/migrations/sqlite/000002_create_posts_table.up.sql
+++ backend/database/migrations/sqlite/000002_create_posts_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE posts (
     id_group INTEGER,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    privacy VARCHAR(50) NOT NULL CHECK (privacy IN ('public', 'private', 'almost private')),
+    privacy VARCHAR(50) NOT NULL CHECK (privacy IN ('public', 'private', 'almost private', 'group')),
     image_name VARCHAR(120),
     created_at DATETIME NOT NULL,
     FOREIGN KEY (id_author) REFERENCES users(id),
