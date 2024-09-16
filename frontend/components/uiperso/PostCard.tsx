d47b8eea4ccec17c31f364014ff2fafb253ec963
cnzale <Azertdev>
2024-09-16 18:20:15 +0000

    sizes attr added in Image balise

--- frontend/components/uiperso/PostCard.tsx
+++ frontend/components/uiperso/PostCard.tsx
@@ -147,6 +147,7 @@ export default function PostCard({
                 src={`/upload/${imageSrc}`}
                 alt={title}
                 fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className="object-cover rounded-lg"
               />
             </div>
